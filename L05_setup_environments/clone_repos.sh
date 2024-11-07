6041f5a84b600707d88666e07e7826725c97db31
Francesco L <francesco.lescai@unipv.it>
2024-11-07 14:04:34 +0100

    changed code for cloning

--- L05_setup_environments/clone_repos.sh
+++ L05_setup_environments/clone_repos.sh
@@ -6,7 +6,7 @@
 
 cd /workspaces/class-variantcalling
 
-git clone https://github.com/lescai-teaching/class-bsa.git
+git clone https://github.com/lescai-teaching/clinical-bioinformatics.git
 git clone https://github.com/lescai-teaching/datasets-class-variantcalling.git
 git clone https://github.com/lescai-teaching/datasets_reference_only.git
 
@@ -15,18 +15,3 @@ git add *
 git commit -m "adding data to repository"
 git push
 
-
-###########################################
-## for the environment of RNA sequencing  #
-###########################################
-
-cd /workspaces/class-rnaseq
-
-git clone https://github.com/lescai-teaching/class-bsa.git
-git clone https://github.com/lescai-teaching/datasets-class-rnaseq.git
-git clone https://github.com/lescai-teaching/datasets_reference_only.git
-
-rm -rf */.git
-git add *
-git commit -m "adding data to repository"
-git push
